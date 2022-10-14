7cd94bd13de31f578bbba92addfc186bc5123cad
junios <junios@naver.com>
2022-10-14 16:55:05 +0900

    사각형 움직이기

--- main.cpp
+++ main.cpp
@@ -14,20 +14,28 @@ int SDL_main(int argc, char* argv[])
 
 	SDL_Window* MyWindow = SDL_CreateWindow("MyGame", 100, 100, 600, 600, SDL_WINDOW_VULKAN);
 	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+
+	int X = 0;
+	while (1)
+	{
+		//화면 지우기
+		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0, 0, 0);
+		SDL_RenderClear(MyRenderer);
+
+		//사각형 그리기
+		if (X > 500)
+		{
+			X = 0;
+		}
+		SDL_SetRenderDrawColor(MyRenderer, 0x00, 0, 0xff, 0);
+		SDL_Rect MyRect = { X++, 100, 100, 100 };
+		SDL_RenderFillRect(MyRenderer, &MyRect);
+
+		//화면에 그리기
+		SDL_RenderPresent(MyRenderer);
+	}
+
 	
-	//화면 지우기
-	SDL_SetRenderDrawColor(MyRenderer, 0xff, 0, 0, 0);
-	SDL_RenderClear(MyRenderer);
-
-	//사각형 그리기
-	SDL_SetRenderDrawColor(MyRenderer, 0x00, 0, 0xff, 0);
-	SDL_Rect MyRect = { 100, 100, 100, 100 };
-	SDL_RenderFillRect(MyRenderer, &MyRect);
-
-	//화면에 그리기
-	SDL_RenderPresent(MyRenderer);
-
-	while(1);
 
 	SDL_DestroyRenderer(MyRenderer);
 	SDL_DestroyWindow(MyWindow);
