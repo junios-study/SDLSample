a380567a90b27b3c06a7be876cec648c49669c2d
junios <junios@naver.com>
2022-10-17 10:16:28 +0900

    이벤트 처리

--- main.cpp
+++ main.cpp
@@ -16,14 +16,13 @@ int SDL_main(int argc, char* argv[])
 	SDL_Renderer* MyRenderer = SDL_CreateRenderer(MyWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 
 	int X = 0;
+	SDL_Color BackgroundColor = { 0xff, 0, 0 ,0 };
 	while (1)
 	{
 		//화면 지우기
-		SDL_SetRenderDrawColor(MyRenderer, 0xff, 0, 0, 0);
+		SDL_SetRenderDrawColor(MyRenderer, BackgroundColor.r, BackgroundColor.g, BackgroundColor.b, BackgroundColor.a);
 		SDL_RenderClear(MyRenderer);
 
-		//Actor ->Render()
-		//사각형 그리기
 		if (X > 500)
 		{
 			X = 0;
@@ -32,11 +31,22 @@ int SDL_main(int argc, char* argv[])
 		SDL_Rect MyRect = { X++, 100, 100, 100 };
 		SDL_RenderFillRect(MyRenderer, &MyRect);
 
+		SDL_Event MyEvent;
+		SDL_PollEvent(&MyEvent);
+
+		if (MyEvent.type == SDL_QUIT)
+		{
+			break;
+		}
+		else if (MyEvent.type == SDL_KEYDOWN)
+		{
+			BackgroundColor = { 0xff, 0xff, 0, 0 };
+		}
+
 		//화면에 그리기
 		SDL_RenderPresent(MyRenderer);
 	}
 
-	
 
 	SDL_DestroyRenderer(MyRenderer);
 	SDL_DestroyWindow(MyWindow);
